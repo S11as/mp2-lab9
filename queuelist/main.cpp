9a2e9946ef827627b0e30ce98e95b033e727d3d4
S11as <50956430+S11as@users.noreply.github.com>
2020-11-29 17:53:18 +0300

    queue

--- queuelist/main.cpp
+++ queuelist/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
-
+#include "TQueueList.h"
 int main()
 {
-    std::cout<<"Hello world"<<std::endl;
+    TQueueList<int> s;
+    for (int i = 0; i < 20; ++i) {
+        s.push(i);
+    }
+    s.save("trp.txt");
     return 0;
 }
---
+++ queuelistlib/TListIterator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Kirill on 11/16/2020.
+//
+
+#include "TListIterator.h"
